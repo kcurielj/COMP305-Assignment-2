cfc858fc82f60c8d74e9ab5b6a8bf2f7529de4de
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2022-08-09 21:12:55 -0500

    Visual changes for FloatingPlatform and CollapsablePlatform

--- Assets/Scripts/CollapseablePlatform.cs
+++ Assets/Scripts/CollapseablePlatform.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CollapseablePlatform : MonoBehaviour
 {
+    public Sprite undamagedPlataform;
+
+    public Sprite damagedPlataform;
+
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
@@ -15,11 +25,19 @@ public class CollapseablePlatform : MonoBehaviour
 
     IEnumerator onDisappear()
     {
+
+        yield return new WaitForSeconds(3f);
+
+        spriteRenderer.sprite = damagedPlataform;
+
         yield return new WaitForSeconds(3f);
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
 
         yield return new WaitForSeconds(3f);
+
+        spriteRenderer.sprite = undamagedPlataform;
+
         gameObject.GetComponent<SpriteRenderer>().enabled = true;
         gameObject.GetComponent<BoxCollider2D>().enabled = true;
 
